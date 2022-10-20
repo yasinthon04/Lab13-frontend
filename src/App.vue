7b6e3fc6ecd5b7624d039b3a55b0d0249178a49f
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-10-20 23:21:22 +0700

    6.8

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,9 @@
   <nav>
     <router-link :to="{ name: 'EventList' }">Home</router-link> |
     <router-link :to="{ name: 'about' }">About</router-link> |
-    <router-link :to="{ name: 'AddEvent' }"> New Event</router-link>
+    <span v-if="isAdmin">
+      <router-link :to="{ name: 'AddEvent' }"> New Event</router-link>
+    </span>
   </nav>
   <router-view />
 </template>
@@ -45,6 +47,9 @@ export default {
   computed: {
     currentUser() {
       return localStorage.getItem('user')
+    },
+    isAdmin() {
+      return AuthService.hasRoles('ROLE_ADMIN')
     }
   },
   methods: {
