15f7b5c0eb381eff04789772f8501896a281d963
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-10-20 21:20:57 +0700

    3.2

---
+++ src/plugins/font-awesome.js
@@ -0,0 +1,13 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomwIcon} from '@fortawesome/vue-fontawesome'
+import { from } from 'core-js/core/array'
+import {
+    faHome,
+    faUser,
+    faUserPlus,
+    faSignInAlt,
+    faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons'
+library.add(faHome,faHome,faUserPlus,faSignInAlt,faSignOutAlt)
+
+export {FontAwesomeIcon}
