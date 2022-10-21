d345e59f652dfacee131187dafd594dd85a060c1
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-10-21 09:15:41 +0700

    docker

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import GStore from '@/store'
 import EventService from '@/services/EventService'
 import OrganizerService from '@/services/OrganizerService.js'
 import Login from '@/views/LoginFormView.vue'
+import Register from '@/views/RegisterFormView.vue'
 const routes = [
   {
     path: '/',
@@ -92,6 +93,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
   {
     path: '/:catchAll(.*)',
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -24,19 +24,26 @@ export default {
   },
   getUser() {
     return JSON.parse(localStorage.getItem('user'))
-},
-hasRoles(roles) {
-  if (GStore.currentUser && roles) {
-    let containRoles = GStore.currentUser.authorities.filter((authority) =>
-      roles.includes(authority)
-    )
-    if (containRoles.length > 0) {
-      return true
+  },
+  hasRoles(roles) {
+    if (GStore.currentUser && roles) {
+      let containRoles = GStore.currentUser.authorities.filter((authority) =>
+        roles.includes(authority)
+      )
+      if (containRoles.length > 0) {
+        return true
+      } else {
+        return false
+      }
     } else {
       return false
     }
-  } else {
-    return false
-  }
+  },
+  register(user) {
+    return apiClient.post('/register', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    })
   }
 }
